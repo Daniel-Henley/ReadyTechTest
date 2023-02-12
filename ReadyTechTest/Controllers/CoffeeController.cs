0e516ab3e3bd35656feb255f95c2dc26a65082bb
Daniel-Henley <52811376+Daniel-Henley@users.noreply.github.com>
2023-02-13 08:40:37 +1100

    WIP

--- ReadyTechTest/Controllers/CoffeeController.cs
+++ ReadyTechTest/Controllers/CoffeeController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using ReadyTechTest.API.Shared;
 using ReadyTechTest.API.Models;
 using Swashbuckle.AspNetCore.Annotations;
+using Newtonsoft.Json;
+using Swashbuckle.Swagger;
 
 namespace ReadyTechTest.API.Controllers;
 
@@ -12,9 +14,10 @@ public class CoffeeController : ControllerBase
     //would configure in appsettings if likely to change
     private const int maxCount = 5;
 
-    public CoffeeController(ILogger<CoffeeController> logger)
+    public CoffeeController(ILogger<CoffeeController> logger, HttpClient httpClient)
     {
         _logger = logger;
+        _httpClient = httpClient;
     }
 
     [SwaggerResponse(StatusCodes.Status200OK,
@@ -26,7 +29,7 @@ public class CoffeeController : ControllerBase
     [SwaggerResponse(StatusCodes.Status418ImATeapot,
                      "April fools!")]
     [HttpGet("brew-coffee")]
-    public ActionResult<CoffeeResponse> BrewCoffee()
+    public async Task<ActionResult<CoffeeResponse>> BrewCoffeeAsync()
     {
         //I'd generally use something like Mediatr to handle requests, but doesn't seem worthwhile for one endpoint
         var date = SystemTime.Now();
@@ -42,6 +45,23 @@ public class CoffeeController : ControllerBase
             return new StatusCodeResult(StatusCodes.Status503ServiceUnavailable);
         }
 
-        return new CoffeeResponse(Constants.CoffeeResponseText, date);
+        //generally put this in as a user secret
+        var apiKey = "a0de4d9ee194eb716c8e52c5046952af";
+        //would set these up as a appsetting
+        var lat = -37.81;
+        var lon = 144.96;
+
+        var queryString = $"weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
+
+        _httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+        var weatherResponse = await _httpClient.GetStringAsync(queryString);
+
+        //for most cases i'd use non anonymous objects but we only need 1 field from the response
+        dynamic weather = JsonConvert.DeserializeObject(weatherResponse);
+        float currentTemp = weather.main.temp;
+        var response = currentTemp > 30f ? new CoffeeResponse(Constants.IcedCoffeeResponseText, date)
+                                        : new CoffeeResponse(Constants.CoffeeResponseText, date);
+
+        return response;
     }
 }
--- ReadyTechTest/Shared/Constants.cs
+++ ReadyTechTest/Shared/Constants.cs
@@ -2,6 +2,7 @@
 public static class Constants
 {
     public const string CoffeeResponseText = "Your piping hot coffee is ready";
+    public const string IcedCoffeeResponseText = "Your refreshing iced coffee is ready";
     public const string ContentTypeApplicationJson = "application/json";
     //would probably just find an existing library for these if we needed more
     public const int April = 4;
